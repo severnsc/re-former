fa11e052a7fb225869b0d5ecfed59fa4c44241d4
Chris Severns <severnsc@gmail.com>
2017-01-30 11:55:20 -0500

    Add Update action to Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
 	def create
 		#@user = User.new(username: params[:username], email: params[:email], password: params[:password])
 		@user = User.new(user_params)
@@ -12,6 +16,12 @@ class UsersController < ApplicationController
 		else
 			render :new
 		end
+	end
+
+	def update
+		@user = User.find(params[:id])
+		@user.update!(user_params)
+		redirect_to new_user_path
 	end
 
 	private
